b25a06ea02e8aa5af60aec31fe37c942c3f2d56c
pwgtiluser.email <87475830+pwgtil@users.noreply.github.com>
2023-05-18 12:37:33 +0200

    Stage 2/5: The user is the gamemaster v2.0

--- task/tictactoe.py
+++ task/tictactoe.py
@@ -1,1 +1,9 @@
-print("X O X\nO X O\nX X O")
+def set_board(grid):
+    print("---------")
+    print(f"| {grid[0]} {grid[1]} {grid[2]} |")
+    print(f"| {grid[3]} {grid[4]} {grid[5]} |")
+    print(f"| {grid[6]} {grid[7]} {grid[8]} |")
+    print("---------")
+
+
+set_board(input())
